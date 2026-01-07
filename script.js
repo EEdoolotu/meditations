8420f317d4c09439a30340c67310d09f44bda5bd
EEdoolotu <ejirogheneedoolotu@gmail.com>
2026-01-07 17:43:39 +0000

    final

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ const verse = document.getElementById("verse-text")
 const verseRef = document.getElementById("verse-ref")
 const commentary = document.getElementById("commentary-text")
 
+let availableList = []
+let masterList = []
+
 
 async function extractVerse() {
     try {
         const response = await fetch("verses.json")
-        const database = await response.json();
+        let database = await response.json();
+        masterList = [...database];
 
         const randomIndex = Math.floor(Math.random() * database.length)
-        const selected = database[randomIndex]
+        let pulledItemArray = masterList.splice(randomIndex,1)
+        const selected = pulledItemArray[0]
 
         verse.innerText = `${selected.verse}`;
         verseRef.innerText = `${selected.reference}`;
         commentary.innerText = `${selected.commentary}`
+        usedVerses.push(selected)
 
-
+        if (masterList.length === 0) {
+            masterList = usedVerses;
+            usedVerses = [];
+        }
 
     } catch (error) {
         console.error("Error loading the scripture database:", error);
